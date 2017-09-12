841396bd4917c783345ae634f6b2ddb28d653843
CharlieGoodson <max.sadchikov@gmail.com>
2017-09-12 16:53:20 +0300

    one more...

--- src/main/java/com/maxsadchikov/address/controller/PersonOverviewController.java
+++ src/main/java/com/maxsadchikov/address/controller/PersonOverviewController.java
@@ -2,11 +2,10 @@ package com.maxsadchikov.address.controller;
 
 import com.maxsadchikov.address.MainApp;
 import com.maxsadchikov.address.model.Person;
+import com.maxsadchikov.address.util.DateUtil;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -32,6 +31,8 @@ public class PersonOverviewController implements Initializable {
     private Label cityLabel;
     @FXML
     private Label birthdayLabel;
+    @FXML
+    private Button deleteBtn;
 
     private MainApp mainApp;
 
@@ -43,11 +44,57 @@ public class PersonOverviewController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
         lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+
+        // Очистка дополнительной информации об адресате.
+        showPersonDetails(null);
+
+        // Слушаем изменения выбора, и при изменении отображаем
+        // дополнительную информацию об адресате.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
     }
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
 
         personTable.setItems(mainApp.getPersonData());
+    }
+
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            // Заполняем метки информацией из объекта person.
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+            birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+
+        } else {
+            // Если Person = null, то убираем весь текст.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
+    }
+
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            // Ничего не выбрано.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+
+            alert.showAndWait();
+        }
     }
 }
